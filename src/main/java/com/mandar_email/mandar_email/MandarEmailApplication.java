88df63909ba1407e5b6df5b9b3ab59ad44eeb2a3
caiovazeredo <caio.viana.39@gmail.com>
2025-07-22 09:42:12 -0300

    adição do projeto

---
+++ src/main/java/com/mandar_email/mandar_email/MandarEmailApplication.java
@@ -0,0 +1,13 @@
+package com.mandar_email.mandar_email;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MandarEmailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MandarEmailApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mandar_email/mandar_email/MandarEmailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mandar_email.mandar_email;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MandarEmailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
